974431a14261761e80ed4f20c69cf4c24a381828
IliusSagar <sagar01830@gmail.com>
2021-12-07 22:15:48 -0800

    Category edit,softdelete & prmenant delete

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -15,6 +15,16 @@
 
                 <div class="col-md-8">
                     <div class="card">
+
+                      @if(session('success'))
+                      <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success') }}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </div>
+                      @endif
+
                         <div class="card-header">All Category</div>
                     
 
@@ -22,22 +32,42 @@
                     <thead>
                       <tr>
                         <th scope="col">SL No</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
+                        <th scope="col">Category Name</th>
+                        <th scope="col">User</th>
                         <th scope="col">Created At</th>
+                        <th scope="col">Action</th>
                       </tr>
                     </thead>
                     <tbody>
 
+                      {{-- @php($i = 1) --}}
+                      @foreach($categories as $category)
                       <tr>
-                        <th scope="row"></th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
+                        <th scope="row">{{ $categories->firstItem()+$loop->index }}</th>
+                        <td>{{ $category->category_name }}</td>
+                        <td>{{ $category->user->name }}</td>
+                        <td>
+                          @if($category->created_at == NULL)
+                          <span class="text-danger"> No Date Set</span>
+                          @else
+                          {{ Carbon\Carbon::parse($category->created_at)->diffforHumans() }}
+                          @endif
+                        </td>
+                        <td>
+                          <a href="{{ url('category/edit/'.$category->id) }}" class="btn btn-info">Edit</a>
+                          <a href="{{ url('softdelete/category/'.$category->id) }}" class="btn btn-danger">Delete</a>
+
+                        </td>
+
+
+
                       </tr>
+                      @endforeach
                            
                     </tbody>
                   </table>
+
+                  {{ $categories->links() }}
 
                 </div>
             </div>
@@ -48,10 +78,15 @@
                     <div class="card-header">Add Category</div>
                         <div class="card-body">
 
-                    <form>
+                    <form action="{{ route('store.category') }}" method="POST">
+                      @csrf
                         <div class="form-group">
                           <label for="exampleInputEmail1">Category Name</label>
-                          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                          <input type="text" name="category_name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+
+                            @error('category_name')
+                              <span class="text-danger">{{ $message }}</span>
+                            @enderror
                           
                         </div>
                         
@@ -64,6 +99,85 @@
 
             </div>
         </div>
+
+
+
+
+
+
+
+
+
+        {{-- Trash Part --}}
+
+
+
+        <div class="container">
+          <div class="row">
+
+              <div class="col-md-8">
+                  <div class="card">
+
+                   
+
+                      <div class="card-header">Trash List</div>
+                  
+
+              <table class="table">
+                  <thead>
+                    <tr>
+                      <th scope="col">SL No</th>
+                      <th scope="col">Category Name</th>
+                      <th scope="col">User</th>
+                      <th scope="col">Created At</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                    {{-- @php($i = 1) --}}
+                    @foreach($trashCat as $category)
+                    <tr>
+                      <th scope="row">{{ $categories->firstItem()+$loop->index }}</th>
+                      <td>{{ $category->category_name }}</td>
+                      <td>{{ $category->user->name }}</td>
+                      <td>
+                        @if($category->created_at == NULL)
+                        <span class="text-danger"> No Date Set</span>
+                        @else
+                        {{ Carbon\Carbon::parse($category->created_at)->diffforHumans() }}
+                        @endif
+                      </td>
+                      <td>
+                        <a href="{{ url('category/restore/'.$category->id) }}" class="btn btn-info">Restore</a>
+                        <a href="{{ url('pdelete/category/'.$category->id) }}" class="btn btn-danger">P Delete</a>
+
+                      </td>
+
+
+
+                    </tr>
+                    @endforeach
+                         
+                  </tbody>
+                </table>
+
+                {{ $trashCat->links() }}
+
+              </div>
+          </div>
+
+
+          <div class="col-md-4">
+              
+          </div>
+
+          </div>
+      </div>
+
+
+      {{-- End Trash --}}
+
 
 
 
